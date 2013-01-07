3080c9242ec4bcfb649d4c49cec5549b6e08afc0
Loic Sharma <sharma.loic@gmail.com>
2013-01-07 15:55:57 -0500

    Improving the settings plugin.

--- modules/settings/plugin.php
+++ modules/settings/plugin.php
@@ -2,11 +2,36 @@
 
 class SettingsPlugin extends Plugin {
 
-	public function __get($key)
+	/**
+	 * Retrieve a setting.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $default
+	 * @return mixed
+	 */
+	public function get($key, $default = null)
 	{
-		return Settings::get($key, null);
+		return Settings::get($key, $default);
 	}
 
+	/**
+	 * Retrieve a setting.
+	 *
+	 * @param  string  $key
+	 * @return mixed
+	 */
+	public function __get($key)
+	{
+		return Settings::get($key);
+	}
+
+	/**
+	 * Retrieve a setting.
+	 *
+	 * @param  string  $method
+	 * @param  array   $args
+	 * @return mixed
+	 */
 	public function __call($method, $args = array())
 	{
 		$default = empty($args) ? null : $args[0];
