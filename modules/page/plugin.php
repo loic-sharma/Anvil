b99a6afb0892facb8c913d234894e640b7bfee3e
Loic Sharma <sharma.loic@gmail.com>
2013-01-07 15:39:47 -0500

    Cleaning up the page plugin.

--- modules/page/plugin.php
+++ modules/page/plugin.php
@@ -2,13 +2,46 @@
 
 class PagePlugin {
 
+	/**
+	 * The current template layout.
+	 *
+	 * @var string
+	 */
 	public $layout = 'layouts.default';
 
+	/**
+	 * The current template.
+	 *
+	 * @var string
+	 */
 	public $template;
+
+	/**
+	 * The page's title.
+	 *
+	 * @var string
+	 */
 	public $title;
+
+	/**
+	 * The page's breadcrumbs
+	 *
+	 * @var array
+	 */
 	public $breadcrumbs = array();
+
+	/**
+	 * The page's content.
+	 *
+	 * @var string
+	 */
 	public $content;
 
+	/**
+	 * Get the page's settings.
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
 		$this->template = Settings::get('template');
@@ -17,6 +50,13 @@ class PagePlugin {
 		$this->addBreadcrumb('Home', '/');
 	}
 
+	/**
+	 * Add a breadcrumb to the page.
+	 *
+	 * @param  string  $name
+	 * @param  string  $link
+	 * @return void
+	 */
 	public function addBreadcrumb($name, $link = null)
 	{
 		if( ! is_null($link))
@@ -30,46 +70,90 @@ class PagePlugin {
 		$this->breadcrumbs[] = (object) compact('name', 'link');
 	}
 
+	/**
+	 * Set the page's content.
+	 *
+	 * @param  string  $view
+	 * @param  array   $data
+	 * @return void
+	 */
 	public function setContent($view, $data = array())
 	{
 		$this->content = View::make($view, $data);
 	}
 
+	/**
+	 * Render the page.
+	 *
+	 * @return string
+	 */
 	public function render()
 	{
-		if(file_exists('templates/'.$this->template.'/bootstrap.php'))
+		$path = App::make('template.path');
+
+		if(file_exists($path.'/start.php'))
 		{
-			include 'templates/'.$this->template.'/bootstrap.php';
+			include $path.'/start.php';
 		}
 
 		return View::make($this->layout);
 	}
 
+	/**
+	 * Fetch the page's title.
+	 *
+	 * @return string
+	 */
 	public function title()
 	{
 		return $this->title;
 	}
 
+	/**
+	 * Fetch the page's content.
+	 *
+	 * @return string
+	 */
 	public function content()
 	{
 		return $this->content;
 	}
 
+	/**
+	 * Determine if the current page is the home page.
+	 *
+	 * @return bool
+	 */
 	public function isHome()
 	{
 		return App::make('controller.router')->isHome();
 	}
 
+	/**
+	 * Determine if the current page has breadcrumbs.
+	 *
+	 * @return bool
+	 */
 	public function hasBreadcrumbs()
 	{
 		return ! empty($this->breadcrumbs);
 	}
 
+	/**
+	 * Fetch the page's breadcrumbs.
+	 *
+	 * @return array
+	 */
 	public function breadcrumbs()
 	{
 		return $this->breadcrumbs;
 	}
 
+	/**
+	 * Fetch the page's load time.
+	 *
+	 * @return double
+	 */
 	public function loadTime()
 	{
 		return round(microtime(true) - CMS_START, 4);
