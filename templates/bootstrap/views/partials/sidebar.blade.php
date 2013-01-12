dca9247cd3ff008e1352408e3752604c4d6cfef7
Loic Sharma <sharma.loic@gmail.com>
2013-01-11 21:23:38 -0500

    Adding the sidebar navigation

--- templates/bootstrap/views/partials/sidebar.blade.php
+++ templates/bootstrap/views/partials/sidebar.blade.php
@@ -1,7 +1,10 @@
 <div class="span4">
   <div class="well">
-    <h3>Sidebar</h3>
+    <h3>Links</h3>
 
+    {{ $navigation->links('sidebar') }}
+
+    <h3>Information</h3>
     <p>Load time: {{ $page->loadTime() }} seconds.</p>
   </div>
 </div>
