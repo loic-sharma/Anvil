3a6421148950d3bdef2742dd232c41d4a91d93e0
Loic Sharma <sharma.loic@gmail.com>
2013-01-13 01:22:27 -0500

    Fixing bug in view.

--- modules/blog/views/post.blade.php
+++ modules/blog/views/post.blade.php
@@ -1,7 +1,7 @@
 <h1>{{ $post->title }}</h1>
 
 <p class="muted">Posted by {{ $post->author->displayName() }} three days ago.</p>
-<p>This is the post!</p>
+<p>{{ $post->content }}</p>
 <p><span class="badge">{{ $post->comments->count() }} Comments</span></p>
 
 @if($post->comments_enabled)
