e91485cf9db8269f56bd8f0bd48a52d09dc91662
Loic Sharma <sharma.loic@gmail.com>
2013-01-15 00:01:19 -0500

    Clicking on the logo now redirects to the home page.

--- templates/bootstrap/views/partials/navigation.blade.php
+++ templates/bootstrap/views/partials/navigation.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar">
 	<div class="navbar-inner">
 		<div class="container">
-			<a class="brand" href="#">{{ $page->title() }}</a>
+			<a class="brand" href="{{ $url->base() }}">{{ $page->title() }}</a>
 
 			<div class="navbar-content">
 				<ul class="nav">
