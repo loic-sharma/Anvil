f8264158cf626173cad4399a8b0ed57368b67bb4
Loic Sharma <sharma.loic@gmail.com>
2013-01-21 14:40:45 -0500

    Guests now have a user object.

--- cms/classes/Cms/Auth/AuthManager.php
+++ cms/classes/Cms/Auth/AuthManager.php
@@ -1,7 +1,47 @@
 <?php namespace Cms\Auth;
 
+use Cms\Auth\Models\User;
+use Cms\Auth\Models\Group;
 use Illuminate\Auth\AuthManager as IlluminateAuthManager;
 
 class AuthManager extends IlluminateAuthManager {
 
+	/**
+	 * Determine if the current user is authenticated.
+	 *
+	 * @return bool
+	 */
+	public function check()
+	{
+		return ($this->user()->power > 0);
+	}
+
+	/**
+	 * Determine if the current user is a guest.
+	 *
+	 * @return bool
+	 */
+	public function guest()
+	{
+		return ($this->user()->power <= 0);
+	}
+
+	/**
+	 * Get the currently authenticated user.
+	 *
+	 * @return Illuminate\Auth\UserInterface|null
+	 */
+	public function user()
+	{
+		$user = $this->driver()->user();
+
+		if(is_null($user))
+		{
+			$user = new User;
+
+			$user->group = Group::where('name', 'guest')->first();
+		}
+
+		return $user;
+	}
 }
