295ef7a917b5e6183bfaa10cc699076af3560e09
Loic Sharma <sharma.loic@gmail.com>
2013-01-21 02:01:22 -0500

    Adding more expressive routes for permission administration.

--- modules/users/routes.php
+++ modules/users/routes.php
@@ -7,3 +7,8 @@ Route::get('admin/users/{id}/edit', 'UsersAdminController@getEditUser');
 Route::post('admin/users/{id}/edit', 'UsersAdminController@postEditUser');
 
 Route::get('admin/users/{id}/delete', 'UsersAdminController@getDeleteUser');
+
+Route::get('admin/permissions', 'PermissionAdminController@getIndex');
+
+Route::get('admin/permission/{id}/edit', 'PermissionAdminController@getEditPermission');
+Route::post('admin/permission/{id}/edit', 'PermissionAdminController@postEditPermission');
