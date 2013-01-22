04e2b28e01c1826859b889ea71e54cc99b66dbfe
Loic Sharma <sharma.loic@gmail.com>
2013-01-22 12:00:54 -0500

    Adding an admin filter.

--- modules/users/filters.php
+++ modules/users/filters.php
@@ -8,6 +8,21 @@ Route::addFilter('logged_in', function()
 	}
 });
 
+Route::addFilter('admin', function()
+{
+	if(Auth::check())
+	{
+		$group = Auth::user()->group;
+
+		if($group->name == 'admin' or $group->power >= 100)
+		{
+			return true;
+		}
+	}
+
+	return false;
+});
+
 Route::addFilter('logged_out', function()
 {
 	if(Auth::check())
