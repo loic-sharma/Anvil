9d1caa026f60fb56cb67b3412a72fc4cde4a141e
Loic Sharma <sharma.loic@gmail.com>
2013-01-22 23:26:20 -0500

    Working on the user filters.

--- modules/users/filters.php
+++ modules/users/filters.php
@@ -1,6 +1,16 @@
 <?php
 
-Route::addFilter('logged_in', function()
+/*
+|--------------------------------------------------------------------------
+| Register the User Filters
+|--------------------------------------------------------------------------
+|
+| These filters will allow routes to check if a user is logged in, logged
+| out, or an admin. They can be reused by other modules.
+|
+*/
+
+Route::filter('logged_in', function()
 {
 	if( ! Auth::check())
 	{
@@ -8,7 +18,7 @@ Route::addFilter('logged_in', function()
 	}
 });
 
-Route::addFilter('admin', function()
+Route::filter('admin', function()
 {
 	if(Auth::check())
 	{
@@ -23,10 +33,22 @@ Route::addFilter('admin', function()
 	return false;
 });
 
-Route::addFilter('logged_out', function()
+Route::filter('logged_out', function()
 {
 	if(Auth::check())
 	{
 		return Redirect::to('users/profile');
 	}
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Load The Autoloader
+|--------------------------------------------------------------------------
+|
+| The CMS relies heavily on Composer components. We'll need the autoloader
+| to load those components, and then to load the CMS itself.
+|
+*/
+Route::filter('admin/*', 'admin');
