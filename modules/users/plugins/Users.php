4bab3a4247ea79388d7014148e770190b0232342
Loic Sharma <sharma.loic@gmail.com>
2013-01-23 00:28:27 -0500

    Users plugin now redirect all calls directly to the user model.

--- modules/users/plugins/Users.php
+++ modules/users/plugins/Users.php
@@ -11,4 +11,28 @@ class UsersPlugin {
 	{
 		return User::all();
 	}
+
+	public function __call($method, $args)
+	{
+		switch (count($args))
+		{
+			case 0:
+				return User::$method();
+
+			case 1:
+				return User::$method($args[0]);
+
+			case 2:
+				return User::$method($args[0], $args[1]);
+
+			case 3:
+				return User::$method($args[0], $args[1], $args[2]);
+
+			case 4:
+				return User::$method($args[0], $args[1], $args[2], $args[3]);
+
+			default:
+				return call_user_func_array(array('User', $method, $method), $args);
+		}
+	}
 }
