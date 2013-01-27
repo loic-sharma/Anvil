729afb5e60d776523fd3d464e31e7814cca85bd7
Loic Sharma <sharma.loic@gmail.com>
2013-01-27 02:31:11 -0500

    Cleaning up the Plugin Manager.

--- cms/classes/Cms/Plugins/PluginManager.php
+++ cms/classes/Cms/Plugins/PluginManager.php
@@ -1,15 +1,10 @@
 <?php namespace Cms\Plugins;
 
+use Cms;
+use View;
 use Closure;
 
 class PluginManager {
-
-	/**
-	 * The application instance.
-	 *
-	 * @var Illuminate\Foundation\Application
-	 */
-	protected $app;
 
 	/**
 	 * All of the registered plugins.
@@ -17,17 +12,6 @@ class PluginManager {
 	 * @var array
 	 */
 	protected $plugins = array();
-
-	/**
-	 * Create a new plugin manager instance.
-	 *
-	 * @param  Illuminate\Foundation\Application  $app
-	 * @return void
-	 */
-	public function __construct($app)
-	{
-		$this->app = $app;
-	}
 
 	/**
 	 * Register a plugin.
@@ -40,7 +24,7 @@ class PluginManager {
 		// If we were given a string, let's get an instance of the plugin.
 		if(is_string($class) and class_exists($class))
 		{
-			$class = $this->app->make($class);
+			$class = Cms::make($class);
 		}
 
 		// If the class inherits the Plugin class, let's wrap it around
@@ -51,7 +35,7 @@ class PluginManager {
 		}
 
 		// Register the plugin in the view environment.
-		$this->app['view']->share($plugin, $class);
+		View::share($plugin, $class);
 
 		$this->plugins[$plugin] = $class;
 	}
