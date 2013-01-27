4f3587d787eb653ae9d513e7fa4b1019e7665849
Loic Sharma <sharma.loic@gmail.com>
2013-01-27 02:25:21 -0500

    Started integrating ckeditor.

--- themes/bootstrap/start.php
+++ themes/bootstrap/start.php
@@ -2,6 +2,12 @@
 
 Plugins::get('theme')->addAsset('css', 'css/bootstrap.css');
 
+if(Auth::user()->can('access_admin_panel'))
+{
+	Plugins::get('theme')->addAsset('js', 'js/ckeditor/ckeditor.js');
+	Plugins::get('theme')->addAsset('js', 'js/edit.js');
+}
+
 Menu::filter(function($item)
 {
 	if($item['a.href'] == Url::current())
