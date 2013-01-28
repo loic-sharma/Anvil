985d507a1c3bb027424acc26c761a9b2cd4f7592
Loic Sharma <sharma.loic@gmail.com>
2013-01-28 00:39:29 -0500

    Added message on admin panel when there are no comments.

--- modules/comments/views/admin/home.blade.php
+++ modules/comments/views/admin/home.blade.php
@@ -1,25 +1,29 @@
 <h1>Comments</h1>
 
-<table class="table table-striped table-bordered">
-	<thead>
-		<tr>
-			<th>Area</th>
-			<th>Author</th>
-			<th>Date</th>
-			<th>Action</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach($comments->get() as $comment)
+@if($comments->count() == 0)
+	<p>There are currently no comments.</p>
+@else
+	<table class="table table-striped table-bordered">
+		<thead>
 			<tr>
-				<td>{{ $comment->areaName }}</td>
-				<td>{{ $comment->author->displayName() }}</td>
-				<td>{{ $comment->date() }}</td>
-				<td>
-					<a href="{{ $comment->areaLink }}" class="btn btn-success">View</a>
-					<a href="{{ $url->to('admin/comment/'.$comment->id.'/delete') }}" class="btn btn-danger">Delete</a> 
-				</td>
+				<th>Area</th>
+				<th>Author</th>
+				<th>Date</th>
+				<th>Action</th>
 			</tr>
-		@endforeach
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			@foreach($comments->get() as $comment)
+				<tr>
+					<td>{{ $comment->areaName }}</td>
+					<td>{{ $comment->author->displayName() }}</td>
+					<td>{{ $comment->date() }}</td>
+					<td>
+						<a href="{{ $comment->areaLink }}" class="btn btn-success">View</a>
+						<a href="{{ $url->to('admin/comment/'.$comment->id.'/delete') }}" class="btn btn-danger">Delete</a> 
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+@endif
