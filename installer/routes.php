798d03393544a10db34498e7a1e1b989c1c678e4
Loic Sharma <sharma.loic@gmail.com>
2013-02-19 23:48:33 -0500

    Fleshing out the first step of the installer's prototype

--- installer/routes.php
+++ installer/routes.php
@@ -2,5 +2,36 @@
 
 Route::get('/', function()
 {
-	return View::make('index');
+	$error = false;
+
+	// Check requirements.
+	$base = dirname(dirname(__FILE__));
+
+	$paths = array(
+		$base . '/anvil/storage/cache/',
+		$base . '/anvil/storage/logs/',
+		$base . '/anvil/storage/meta/',
+		$base . '/anvil/storage/sessions/',
+		$base . '/anvil/storage/views/',
+	);
+
+	foreach($paths as $path)
+	{
+		if( ! is_writable($path))
+		{
+			$errors = true;
+
+			echo '<p>'.$path . ' must be writable.</p>';
+		}
+	}
+
+	if($errors == false)
+	{
+		echo '<p><a href="'.URL::to('step-2').'">Next step</a></p>';
+	}
+});
+
+Route::get('step-2', function()
+{
+	echo 'Second step.';
 });
