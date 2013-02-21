7011ffc3dc29a64ec1708957c6b138a94847440b
Loic Sharma <sharma.loic@gmail.com>
2013-02-21 11:42:57 -0500

    Adding a little note on the installer.

--- installer/views/requirements.php
+++ installer/views/requirements.php
@@ -1,4 +1,6 @@
 <?php if(count($errors) == 0): ?>
+	<p>Welcome to Anvil's installer! This is only a prototype, and will be rewritten later.</p>
+
 	<p><a href="<?php echo URL::to('step-2'); ?>">Next step</a></p>
 <?php else: ?>
 	<?php foreach($errors as $error): ?>
