b18467ee8d16baa3988cb5e128b26d4493b4f98f
Loic Sharma <sharma.loic@gmail.com>
2013-02-28 23:32:30 -0500

    Added tests for settings repository

---
+++ anvil/tests/Settings/SettingsRepositoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+class SettingsRepositoryTest extends PHPUnit_Framework_TestCase {
+
+	public function testInstantiation()
+	{
+		$loader = $this->getMock('Anvil\Settings\LoaderInterface');
+		$loader->expects($this->once())
+			->method('get');
+
+		$repository = new Anvil\Settings\Repository($loader);
+	}
+
+	public function testGetSetting()
+	{
+		$settings = array('foo' => 'bar', 'fooz' => 'baz');
+
+		$loader = $this->getMock('Anvil\Settings\LoaderInterface');
+		$loader->expects($this->once())
+			->method('get')
+			->will($this->returnValue($settings));
+
+		$repository = new Anvil\Settings\Repository($loader);
+
+		$this->assertEquals($settings['foo'], $repository->get('foo'));
+		$this->assertEquals('default', $repository->get('fake', 'default'));
+	}
+
+	public function testSetSetting()
+	{
+		$expected = 'foobar';
+
+		$loader = $this->getMock('Anvil\Settings\LoaderInterface');
+		$repository = new Anvil\Settings\Repository($loader);
+
+		$this->assertEquals('default', $repository->get('fake', 'default'));
+
+		$repository->set('fake', $expected);
+
+		$this->assertEquals($expected, $repository->get('fake', 'default'));
+	}
+
+	public function testSaveOnUnset()
+	{
+		$expected = array('foo' => 'bar', 'fooz' => 'baz');
+
+		$loader = $this->getMock('Anvil\Settings\LoaderInterface');
+		$loader->expects($this->once())
+			->method('save')
+			->with($expected);
+
+		$repository = new Anvil\Settings\Repository($loader);
+
+		$repository->set('foo', 'bar');
+		$repository->set('fooz', 'baz');
+
+		unset($repository);
+	}
+}
