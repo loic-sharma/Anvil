d97d80b11925bd1152bfff8808700491adaec602
Loic Sharma <sharma.loic@gmail.com>
2013-04-28 16:09:30 -0400

    Added CRSF filter

--- anvil/filters.php
+++ anvil/filters.php
@@ -195,3 +195,22 @@ Route::filter('admin', function()
 
 Route::when('admin', 'admin');
 Route::when('admin/*', 'admin');
+
+/*
+|--------------------------------------------------------------------------
+| CSRF Protection Filter
+|--------------------------------------------------------------------------
+|
+| The CSRF filter is responsible for protecting your application against
+| cross-site request forgery attacks. If this special token in a user
+| session does not match the one given in this request, we'll bail.
+|
+*/
+
+Route::filter('csrf', function()
+{
+	if (Session::token() != Input::get('_token'))
+	{
+		throw new Illuminate\Session\TokenMismatchException;
+	}
+});
