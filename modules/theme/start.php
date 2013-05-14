7c9c219af837a832b028dcbe940d926f29420ce8
Loic Sharma <sharma.loic@gmail.com>
2013-05-14 17:00:58 -0500

    Simplified the theme module
    Removed unneeded plugins

--- modules/theme/start.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Autoloader::map(array(
-	'ThemePlugin'  => __DIR__.'/plugins/Theme.php',
-	'ThemesPlugin' => __DIR__.'/plugins/Themes.php',
-));
-
-Plugins::register('theme', new ThemePlugin);
-Plugins::register('themes', new ThemesPlugin);
