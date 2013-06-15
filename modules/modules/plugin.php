7e2ea20eef8b0c8001954b591660a4d50627843c
Loic Sharma <sharma.loic@gmail.com>
2013-06-15 00:54:11 -0700

    Fixed modules plugin
    Still needs heavy refactoring.

--- modules/modules/plugin.php
+++ modules/modules/plugin.php
@@ -10,13 +10,13 @@ class ModulesPlugin {
 	public function get()
 	{
 		$modules = array();
-		$paths = glob(Cms::make('module.path').'/*');
+		$paths = glob(Anvil::make('module.path').'/*');
 
 		foreach($paths as $path)
 		{
 			if(file_exists($path.'/module.php'))
 			{
-				$moduleName = str_replace(Cms::make('module.path').'/', '', $path);
+				$moduleName = str_replace(Anvil::make('module.path').'/', '', $path);
 				$moduleName = ucfirst($moduleName).'Module';
 
 				$module = new $moduleName;
