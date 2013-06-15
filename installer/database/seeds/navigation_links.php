4901c5880f8dc9851958a05eb901bedda80807c0
Loic Sharma <sharma.loic@gmail.com>
2013-06-15 00:53:19 -0700

    Added admin links to installer

--- installer/database/seeds/navigation_links.php
+++ installer/database/seeds/navigation_links.php
@@ -1,6 +1,8 @@
 <?php
 
 return array(
+
+	// Main Menu Links
 	array(
 		'menu_id'   => 1,
 		'parent_id' => NULL,
@@ -17,6 +19,8 @@ return array(
 		'max_power' => NULL,
 		'required_power' => NULL,
 	),
+
+	// Sidebar links
 	array(
 		'menu_id'   => 2,
 		'parent_id' => NULL,
@@ -48,5 +52,87 @@ return array(
 		'url'       => '{url}/login',
 		'max_power' => 0,
 		'required_power' => NULL,
+	),
+
+	// Admin Menu Links
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Blog',
+		'url'       => '{adminUrl}/blog',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Comments',
+		'url'       => '{adminUrl}/comments',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Modules',
+		'url'       => '{adminUrl}/modules',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Navigation',
+		'url'       => '{adminUrl}/navigation',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Blog',
+		'url'       => '{adminUrl}/blog',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Page',
+		'url'       => '{adminUrl}/page',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Settings',
+		'url'       => '{adminUrl}/settings',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Theme',
+		'url'       => '{adminUrl}/theme',
+		'max_power' => NULL,
+		'required_power' => 100,
+	),
+
+	array(
+		'menu_id'   => 3,
+		'parent_id' => NULL,
+		'title'     => 'Users',
+		'url'       => '{adminUrl}/users',
+		'max_power' => NULL,
+		'required_power' => 100,
 	),
 );
